c86ebcf57831645596110ffa8aa9e2b28e0dd9e5
vinnyasa <vinnyasa@gmail.com>
2018-06-21 23:19:27 -0700

    added basic setup Node

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var mysql = require('./gitAuth.js');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var request = require('request');
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+app.use(express.static('public'));
+
+app.get('/',function(req,res,next){
+    var context = {};
+
+    res.render('home', context);
+
+});
+
+app.post('/',function(req,res,next){
+    var context = {};
+
+    res.send('home', context);
+
+});
+
+
+app.use(function(req,res){
+    res.status(404);
+    res.render('404');
+});
+
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
