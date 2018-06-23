b6206d62bd4c33859f53e7f7bfd0e66ac8984f93
Luke Blanchard <lucas.blanchard12@gmail.com>
2018-06-22 20:26:00 -0500

    api call in server-side, home.handlebars displays results

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
-var mysql = require('./gitAuth.js');
+//var mysql = require('./gitAuth.js');
+var axios = require('axios'); 
 
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
-var request = require('request');
+//var request = require('request');
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -17,13 +18,55 @@ app.get('/',function(req,res,next){
 
 });
 
+app.get('/repos', function(req, res, next){
+    //search by language
+    var context = {};
+    context.repos = [];
+    var idx = 0;
+    var lang = "python"; //TODO: get language from form req 
+    //search public or private repos
+    var publicPrivate = "public"; 
+    //fork or main repo
+    var fork = false; 
+    //common open source licenses 
+    var licenses = ["mit", "gpl", "apache-2.0", "mpl-2.0", "cc"]; 
+    //sort by stars, forks, or updated
+    var sortBy = "stars"; 
+
+    //Build query string 
+    var url = 'https://api.github.com/search/repositories?q='; 
+    url += "language:" + lang; 
+    licenses.forEach((l) => {
+        url += "+license:" + l
+    }); 
+    url += "+is:" + publicPrivate; 
+    url += "+fork:" + fork; 
+    //url += "+stars:" + starCount;
+    url += "&sort=" + sortBy; 
+    url += "&order=desc"; 
+    axios.get(url)
+        .then(response => {
+            while(context.repos.length < 10 && idx < response.data.items.length){
+                var r = response.data.items[idx]; 
+                if(r.open_issues_count > 0){
+                    context.repos.push(response.data.items[idx]);
+                }
+                idx++;
+            }
+            res.render('home', context);
+        })
+        .catch(error => {
+            console.log(error); 
+            res.render('500');
+        }); 
+}); 
+
 app.post('/',function(req,res,next){
     var context = {};
 
     res.send('home', context);
 
 });
-
 
 app.use(function(req,res){
     res.status(404);
